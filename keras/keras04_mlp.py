ebeee7b0f58e766457cc35fdb77b18c750403485
Ljryong <aa5357@naver.com>
2024-01-02 17:47:40 +0900

    init

--- keras/keras04_mlp.py
+++ keras/keras04_mlp.py
@@ -6,7 +6,6 @@ from keras.layers import Dense
 
 #1. 데이터
 x = np.array([[1,2,3,4,5,6,7,8,9,10],[1,1.1,1.2,1.3,1.4,1.5,1.6,1.5,1.4,1.3]])      #칼럼을 2개로 만들면 괄호를 한개 더 쳐야한다. 칼럼=열    (2행 10열)= 2개의 덩어리 안에 열개씩 있다.
-
 y = np.array([1,2,3,4,5,6,7,8,9,10])            #(1,2,3,4,5,6,7,8,9,10) = (10,) = 스칼라가 10개짜리 한 묶음
 
 print(x.shape)         #(2, 10)  =  (행, 열)          #shape : 모양  (2,10) 2개의 행과 10개의 열의 모양
@@ -16,19 +15,15 @@ x = x.T         # x = x.T 로 하면 행렬 전치가 이루어진다. (2,10)에
 print(x.shape)  #(10,2)
 #[[1,1],[2,1.1],[3,1.3], ... [10,1.3]]
 
-
 #2 모델구성
 model = Sequential()
 model.add(Dense(4,input_dim = 2))           # 열, 컬럼, 속성, 특성, 차원 = 2 // (행무시,열우선) <= 외우기  (행의 갯수, 열의 갯수)
 model.add(Dense(4))
 model.add(Dense(1))
 
-
-
 #3 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
 model.fit(x,y,epochs=7000)
-
 
 #4 평가, 예측
 loss = model.evaluate(x,y)
