f04ede19e488f299e30c2f4e985291fd04c15162
Ljryong <aa5357@naver.com>
2024-01-03 19:52:05 +0900

    init

--- keras/0004.py
+++ keras/0004.py
@@ -4,7 +4,7 @@ from keras.layers import Dense
 
 #1 데이터
 x = np.array(range(1,11))
-y = np.array([1,2,3,4,5,6,8,7,9,10])
+y = np.array([1,2,3,4,5,6,7,8,9,10])
 
 x_train = x[:8]
 y_train = y[:8]
@@ -19,12 +19,14 @@ print(y_test)
 
 #2 모델구성
 model = Sequential()
-model.add(Dense(3,input_dim = 1))
+model.add(Dense(5,input_dim = 1))
+model.add(Dense(6))
+model.add(Dense(3))
 model.add(Dense(1))
 
 #3 컴파일,훈련
 model.compile(loss='mse',optimizer='adam')
-model.fit(x_train , y_train, epochs = 1250, batch_size = 1 )
+model.fit(x_train , y_train, epochs = 1000, batch_size = 4 )
 
 #4 평가, 예측
 loss = model.evaluate(x_test,y_test)
@@ -33,10 +35,23 @@ result = model.predict([12500,9])
 print("로스는 : ", loss)
 print("결과는 : ", result)
 
-
+# 과적합
 # 8/8 [==============================] - 0s 2ms/step - loss: 0.2540
 # 1/1 [==============================] - 0s 67ms/step - loss: 0.0158
 # 1/1 [==============================] - 0s 48ms/step
-# 로스는 :  0.015801550820469856
+# 로스는 :  0.015801550820469856s
 # 결과는 :  [[1.218285e+04]
 #  [8.887629e+00]]
+# 로스값의 차이는 0.24
+
+
+# 과적합이 아닐 때
+# 2/2 [==============================] - 0s 0s/step - loss: 1.0786e-11
+# 1/1 [==============================] - 0s 82ms/step - loss: 3.8654e-11
+# 1/1 [==============================] - 0s 49ms/step
+# 로스는 :  3.865352482534945e-11
+# 결과는 :  [[1.2499984e+04]
+#  [8.9999943e+00]]
+# 로스값의 차이는 약 0.000000000002 정도
+
+# 과적합으로 나타는 로스값의 차이는 매우 크다
