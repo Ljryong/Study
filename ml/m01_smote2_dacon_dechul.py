dd6160aa33e8eba5956672c0e317f2e582d15d9f
Ljryong <aa5357@naver.com>
2024-01-30 11:40:25 +0900

    pull

--- ml/m01_smote2_dacon_dechul.py
+++ ml/m01_smote2_dacon_dechul.py
@@ -70,7 +70,7 @@ filepath = ''.join([path , 'k28_11_', date , '_', filename ])
 
 
 
-train_csv['주택소유상태'] = train_csv['주택소유상태'].replace({'MORTGAGE' : 0 , 'OWN' : 1 , 'RENT': 2 , 'ANY' : 3}).astype(float)
+train_csv['주택소유상태'] = train_csv['주택소유상태'].replace({'MORTGAGE' : 0 , 'OWN' : 1 , 'RENT': 2 , 'ANY' : 0}).astype(float)
 test_csv['주택소유상태'] = test_csv['주택소유상태'].replace({'MORTGAGE' : 0 , 'OWN' : 1 , 'RENT': 2}).astype(float)
 
 train_csv['대출목적'] = train_csv['대출목적'].replace({'부채 통합' : 0 , '주택 개선' : 2 , '주요 구매': 4 , '휴가' : 9  
@@ -79,7 +79,7 @@ train_csv['대출목적'] = train_csv['대출목적'].replace({'부채 통합' :
 test_csv['대출목적'] = test_csv['대출목적'].replace({'부채 통합' : 0 , '주택 개선' : 2 , '주요 구매': 4 , '휴가' : 9 ,
                                              '의료' : 5 , '자동차' : 6 , '신용 카드' : 1 , '기타' : 3 , '주택개선' : 8,
                                              '소규모 사업' : 7 , '이사' :  8 , '주택': 10 , '재생 에너지' : 11 , 
-                                             '결혼' : 12 })
+                                             '결혼' : 0 })
 
 # 결혼은 train에 없는 라벨이다. 그래서 12 로 두든 2로 두든 아니면 없애든 값이 좋은걸로 비교해보면 된다.
 train_csv['대출기간'] = train_csv['대출기간'].replace({' 36 months' : 36 , ' 60 months' : 60 }).astype(int)
@@ -97,6 +97,9 @@ test_csv['근로기간'] = test_csv['근로기간'].replace({'10+ years' : 10 , 
                                                       '6 years' : 6 , '7 years' : 7 , '9 years' : 9 , '10+years' : 11,
                                                       '<1 year' : 0.5 , '3' : 3 , '1 years' : 1.5 })
 
+
+encoder.fit(train_csv['대출등급'])
+train_csv['대출등급'] = encoder.transform(train_csv['대출등급'])
 
 # print(train_csv['대출기간'])
 
@@ -116,15 +119,27 @@ y = train_csv['대출등급']
 
 y = y.values.reshape(-1,1)       # (96294, 1)
 
+
 # print(y.shape) 
 ohe = OneHotEncoder(sparse = False)
 ohe.fit(y)
 y_ohe = ohe.transform(y) 
 
+######################################################
 
-x_train ,x_test , y_train , y_test = train_test_split(x,y_ohe,test_size = 0.3, random_state= 19 , shuffle=True , stratify=y)    # 0 1502
-es = EarlyStopping(monitor='val_loss', mode='min' , patience= 100 , restore_best_weights=True , verbose= 1 )
+# start = time.time()
+# smote = SMOTE(random_state= 451 )
+# x_train , y_train = smote.fit_resample(x , y_ohe )
 
+# # print(pd.value_counts(x_train))
+# # print(pd.value_counts(y_train))
+# end = time.time()
+# print('시간' , end - start)
+
+######################################################
+
+x_train ,x_test , y_train , y_test = train_test_split(x,y_ohe,test_size = 0.3, random_state= 27 , shuffle=True , stratify=y)    # 0 1502
+es = EarlyStopping(monitor='val_acc', mode='max' , patience= 300 , restore_best_weights=True , verbose= 1  )
 
 
 
@@ -145,40 +160,35 @@ x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 test_csv = scaler.transform(test_csv)
 
-######################################################
-
-start = time.time()
-smote = SMOTE(random_state=0)
-x_train, y_train = smote.fit_resample(x_train , y_train)
-
-# print(pd.value_counts(x_train))
-# print(pd.value_counts(y_train))
-end = time.time()
-print('시간' , end - start)
-
-######################################################
-
-
-
-
-#2
+# 2 0.89
 model = Sequential()
-model.add(Dense(102 ,input_shape= (13,)))
-model.add(Dropout(0.3))
-model.add(Dense(15,activation= 'relu'))
-model.add(Dense(132,activation= 'relu'))
-model.add(Dropout(0.3))
-model.add(Dense(13, activation= 'relu'))
-model.add(Dense(64,activation= 'relu'))
-model.add(Dense(7,activation='softmax'))
+model.add(Dense(19, input_shape= (13, ),activation='swish'))
+model.add(Dense(97,activation='swish'))
+model.add(Dropout(0.2))
+model.add(Dense(21,activation='swish'))
+model.add(Dense(32,activation='swish'))
+model.add(Dense(16,activation='swish'))
+model.add(Dense(21,activation='swish'))
+model.add(Dense(7, activation='softmax'))
+
+#2-1 0.89
+# model = Sequential()
+# model.add(Dense(102 ,input_shape= (13,),activation= 'swish'))
+# model.add(Dense(15,activation= 'swish'))
+# model.add(Dense(132,activation= 'swish'))
+# model.add(Dense(13, activation= 'swish'))
+# model.add(Dense(64,activation= 'swish'))
+# model.add(Dense(7,activation='softmax'))
+
 
 #3
 from keras.callbacks import EarlyStopping ,ModelCheckpoint
 mcp = ModelCheckpoint(monitor='val_loss', mode='min' , verbose=1, save_best_only=True , filepath=  filepath   )
 
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-model.fit(x_train,y_train, epochs = 100000 , batch_size= 700 , validation_split=0.2 , callbacks = [es,mcp] , verbose= 2 )
+model.compile(loss='categorical_crossentropy', optimizer='adam' , metrics=['acc'] )
+model.fit(x_train,y_train, epochs = 100000 , batch_size= 500 , validation_split=0.2 , callbacks = [es,mcp] , verbose= 2 )
+
 
 
 #4
@@ -205,7 +215,7 @@ acc = acc(arg_test,arg_pre)
 
 
 
-submission_csv.to_csv(path+'submission_0119.csv', index = False)
+submission_csv.to_csv(path+'submission_0130.csv', index = False)
 
 
 print('y_submit = ', y_submit)
@@ -275,10 +285,10 @@ print("f1 = ",f1)
 # #2
 # model = Sequential()
 # model.add(Dense(102 ,input_shape= (13,)))
-# model.add(Dense(15,activation= 'relu'))
-# model.add(Dense(132,activation= 'relu'))
-# model.add(Dense(13, activation= 'relu'))
-# model.add(Dense(64,activation= 'relu'))
+# model.add(Dense(15,activation= 'swish'))
+# model.add(Dense(132,activation= 'swish'))
+# model.add(Dense(13, activation= 'swish'))
+# model.add(Dense(64,activation= 'swish'))
 # model.add(Dense(7,activation='softmax'))
 # Epoch 3597: early stopping
 # 903/903 [==============================] - 1s 876us/step - loss: 0.2848 - acc: 0.9255
