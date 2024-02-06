59770e6ed775b77edd8a3e78d53cede52da71d83
Ljryong <aa5357@naver.com>
2024-02-06 11:44:29 +0900

    add

--- 연습장/sihum.py
+++ 연습장/sihum.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from keras.models import Sequential , Model
+from keras.models import Sequential , Model , load_model
 from keras.layers import Dense , Input ,concatenate , Conv1D ,Flatten , LSTM
 from keras.callbacks import EarlyStopping , ModelCheckpoint
 from sklearn.model_selection import train_test_split
@@ -12,8 +12,8 @@ from sklearn.preprocessing import LabelEncoder ,MaxAbsScaler , MinMaxScaler ,Rob
 #1 데이터
 path = 'c:/_data/sihum//'
 # csv 파일 가져오면서 , 제거
-train1 = pd.read_csv(path + '삼성240205.csv', encoding='cp949', thousands=',' ,index_col=0 )
-train2 = pd.read_csv(path + '아모레240205.csv',encoding='cp949',thousands=',' ,index_col=0)
+train1 = pd.read_csv(path + '삼성 240205.csv', encoding='cp949', thousands=',' ,index_col=0 )
+train2 = pd.read_csv(path + '아모레 240205.csv',encoding='cp949',thousands=',' ,index_col=0)
 
 # 데이터 수치화 
 train1['전일비'] = train1['전일비'].replace({'▼' : 0 , '▲' : 1 , ' ': 2})
@@ -31,34 +31,44 @@ print(type(train1))         # <class 'pandas.core.frame.DataFrame'>
 print(type(train2))         # <class 'pandas.core.frame.DataFrame'>
 
 
-train1 = train1.fillna(train1['거래량'].mean())
-train1 = train1.fillna(train1['금액(백만)'].mean())
-train2 = train2.fillna(train2['거래량'].mean())
-train2 = train2.fillna(train2['금액(백만)'].mean())
+# train1 = train1.fillna(train1['거래량'].mean())
+# train1 = train1.fillna(train1['금액(백만)'].mean())
+# train2 = train2.fillna(train2['거래량'].mean())
+# train2 = train2.fillna(train2['금액(백만)'].mean())
 
-train1 = train1.sort_values(['일자'],ascending=True)
-train2 = train2.sort_values(['일자'],ascending=True)
-
-train1_test = train1['시가'][-5:]
-train2_test = train2['종가'][-5:]
-
-print(train1_test)
-print(train2_test)
-
-train1 = train1[train1.index > '2018/05/04']
-train2 = train2[train2.index > '2018/05/04']
-
-# train1 = train1.dropna()
-# train2 = train2.dropna()
+train1 = train1.dropna()
+train2 = train2.dropna()
 
 # train1 = train1.fillna(train1.ffill)          # 앞방향으로 똑같은거 채우기
 # train2 = train2.fillna(train2.ffill)  
 # train1 = train1.fillna(train1.bfill)          # 뒷방향으로 똑같은거 채우기
 # train2 = train2.fillna(train2.bfill) 
 
+train1 = train1.sort_values(['일자'],ascending=True)
+train2 = train2.sort_values(['일자'],ascending=True)
+
+# train1_test = train1['시가'][-5:]
+# train2_test = train2['종가'][-5:]
+
+timesteps = 6
+adds = 2
 # 나누기
+def split_xy(dataFrame, cutting_size, y_behind_size,  y_column):
+    xs = []
+    ys = [] 
+    for i in range(len(dataFrame) - cutting_size - y_behind_size):
+        x = dataFrame[i : (i + cutting_size)]
+        y = dataFrame[i + cutting_size + y_behind_size : (i + cutting_size + y_behind_size + 1) ][y_column]
+        xs.append(x)
+        ys.append(y)
+    return (np.array(xs), np.array(ys))
+
+
+train1 = train1[train1.index > '2018/05/04']
+train2 = train2[train2.index > '2018/05/04']
+
 timestep = 6
-add = 1
+add = 2
 def split(train1,train2,timestep,col1,col2,add) : 
     x=[]
     y=[]
@@ -79,10 +89,15 @@ def split(train1,train2,timestep,col1,col2,add) :
     return np.array(x) , np.array(y) , np.array(z) , np.array(f)
 
 
-
-
 x1 , y1 , x2, y2 = split(train1,train2,timestep,'시가','종가',add)
 
+# 샘플 추출 x
+compare_predict_size = 10
+samsung_sample_x = x1[-compare_predict_size:]
+amore_sample_x = x2[-compare_predict_size :]
+# 샘플 추출 y
+samsung_sample_y = np.append(y1[-compare_predict_size + 2:], ["24/02/06 시가","24/02/07 시가"]) 
+amore_sample_y = np.append(y2[-compare_predict_size + 2:], ["24/02/06 종가","24/02/07 종가"])
 # iloc = index 를 제외한 수치(값)들만을 뽑아내준다. 
 
 
@@ -125,6 +140,8 @@ x1_train = x1_train.reshape(-1,96)
 x2_train = x2_train.reshape(-1,96)
 x1_test = x1_test.reshape(-1,96)
 x2_test = x2_test.reshape(-1,96)
+r_samsung_sample_x = samsung_sample_x.reshape(samsung_sample_x.shape[0], samsung_sample_x.shape[1] * samsung_sample_x.shape[2])
+r_amore_sample_x = amore_sample_x.reshape(amore_sample_x.shape[0], amore_sample_x.shape[1] * amore_sample_x.shape[2])
 
 
 # scaler 사용
@@ -137,13 +154,22 @@ x1_train = scaler.fit_transform(x1_train)
 x2_train = scaler.fit_transform(x2_train)
 x1_test = scaler.fit_transform(x1_test)
 x2_test = scaler.fit_transform(x2_test)
+r_samsung_sample_x = scaler.transform(r_samsung_sample_x)
+r_amore_sample_x = scaler.transform(r_amore_sample_x)
 
 x1_train = x1_train.reshape(-1,6,16)
 x2_train = x2_train.reshape(-1,6,16)
 x1_test = x1_test.reshape(-1,6,16)
 x2_test = x2_test.reshape(-1,6,16)
 
-print('데이터 전처리')
+# print()
+
+samsung_sample_x = r_samsung_sample_x.reshape(-1, samsung_sample_x.shape[1], samsung_sample_x.shape[2])
+amore_sample_x = r_amore_sample_x.reshape(-1, amore_sample_x.shape[1], amore_sample_x.shape[2])
+
+print(samsung_sample_x.shape)           # (10, 6, 16, 1)
+print(amore_sample_x.shape)             # (10, 6, 16, 1)
+
 
 #2 모델구성
 #2-1 모델1
@@ -189,11 +215,15 @@ mgd2 = Dense(32,activation='swish')(mgd1)
 mgd3 = Dense(64,activation='swish')(mgd2)
 mgd4 = Dense(16,activation='swish')(mgd3)
 mgd5 = Dense(32,activation='swish')(mgd4)
-mgd6 = Dense(64,activation='swish')(mgd5)
-mgd7 = Dense(16,activation='swish')(mgd6)
-last = Dense(1,activation='swish')(mgd7)
+mgd6 = Dense(8,activation='swish')(mgd5)
+mgd7 = Dense(64,activation='swish')(mgd6)
+mgd8 = Dense(16,activation='swish')(mgd7)
+mgd9 = Dense(32,activation='swish')(mgd8)
+mgd10 = Dense(32,activation='swish')(mgd9)
+mgd11 = Dense(8,activation='swish')(mgd10)
+last = Dense(1,activation='swish')(mgd11)
 
-last2 = Dense(1,activation='swish')(mgd7)
+last2 = Dense(1,activation='swish')(mgd11)
 
 
 model = Model(inputs = [in1,in12] , outputs = [last,last2])
@@ -204,10 +234,12 @@ es = EarlyStopping(monitor='val_loss' , mode = 'min' , restore_best_weights=True
 # modelcheckpoint
 # filepath = 'c:/_data/_save/MCP//'
 # mcp = ModelCheckpoint(monitor='val_loss' , mode = 'min' , save_best_only=True , verbose= 1 , filepath=filepath  )
-model.fit([x1_train,x2_train],[y1_train,y2_train] ,epochs= 10000000 , batch_size= 300 , validation_split=0.2 , callbacks=[es], verbose=2 )
+model.fit([x1_train,x2_train],[y1_train,y2_train] ,epochs= 10000000 , batch_size= 100 , validation_split=0.2 , callbacks=[es], verbose=2 )
 model.save("c:/_data/sihum/sihum_1.h5")
 
 #4 평가, 예측
+print(x1_test.shape,x2_test.shape)  # (423, 6, 16) , (423, 6, 16)
+
 loss = model.evaluate([x1_test,x2_test],[y1_test,y2_test])
 pre = model.predict([x1_test,x2_test])
 
@@ -215,12 +247,26 @@ r21 = r2_score(y1_test,pre[0])
 r22 = r2_score(y2_test,pre[1])
 
 
-for i in range(5):
-    print('실제' , train1_test[i] , '예측', pre[0][i] )
+# for i in range(5):
+#     print('실제' , train1_test[i] , '예측', pre[0][i] )
 
-for i in range(5):
-    print('실제' , train2_test[i] , '예측', pre[1][i] )
+# for i in range(5):
+#     print('실제' , train2_test[i] , '예측', pre[1][i] )
 
+print("="*100)
+sample_dataset_y = [samsung_sample_y,amore_sample_y]
+sample_predict_x = model.predict([samsung_sample_x,amore_sample_x])
+
+
+for i in range(len(sample_dataset_y)):
+    if i == 0 :
+        print("\t\tSAMSUNG\t시가")
+    else:
+        print("="*100)
+        print("\t\tAMORE\t종가")
+    for j in range(compare_predict_size):
+        print(f"\tD-{compare_predict_size - j  - 1}: {sample_dataset_y[i][j]}\t예측값 {np.round(sample_predict_x[i][j])}\t")
+print("="*100)
 
 
 print('loss',loss)
--- 연습장/sihum_load.py
+++ 연습장/sihum_load.py
@@ -12,8 +12,8 @@ from sklearn.preprocessing import LabelEncoder ,MaxAbsScaler , MinMaxScaler ,Rob
 #1 데이터
 path = 'c:/_data/sihum//'
 # csv 파일 가져오면서 , 제거
-train1 = pd.read_csv(path + '삼성240205.csv', encoding='cp949', thousands=',' ,index_col=0 )
-train2 = pd.read_csv(path + '아모레240205.csv',encoding='cp949',thousands=',' ,index_col=0)
+train1 = pd.read_csv(path + '삼성 240205.csv', encoding='cp949', thousands=',' ,index_col=0 )
+train2 = pd.read_csv(path + '아모레 240205.csv',encoding='cp949',thousands=',' ,index_col=0)
 
 # 데이터 수치화 
 train1['전일비'] = train1['전일비'].replace({'▼' : 0 , '▲' : 1 , ' ': 2})
@@ -31,34 +31,44 @@ print(type(train1))         # <class 'pandas.core.frame.DataFrame'>
 print(type(train2))         # <class 'pandas.core.frame.DataFrame'>
 
 
-train1 = train1.fillna(train1['거래량'].mean())
-train1 = train1.fillna(train1['금액(백만)'].mean())
-train2 = train2.fillna(train2['거래량'].mean())
-train2 = train2.fillna(train2['금액(백만)'].mean())
+# train1 = train1.fillna(train1['거래량'].mean())
+# train1 = train1.fillna(train1['금액(백만)'].mean())
+# train2 = train2.fillna(train2['거래량'].mean())
+# train2 = train2.fillna(train2['금액(백만)'].mean())
 
-train1 = train1.sort_values(['일자'],ascending=True)
-train2 = train2.sort_values(['일자'],ascending=True)
-
-train1_test = train1['시가'][-5:]
-train2_test = train2['종가'][-5:]
-
-print(train1_test)
-print(train2_test)
-
-train1 = train1[train1.index > '2018/05/04']
-train2 = train2[train2.index > '2018/05/04']
-
-# train1 = train1.dropna()
-# train2 = train2.dropna()
+train1 = train1.dropna()
+train2 = train2.dropna()
 
 # train1 = train1.fillna(train1.ffill)          # 앞방향으로 똑같은거 채우기
 # train2 = train2.fillna(train2.ffill)  
 # train1 = train1.fillna(train1.bfill)          # 뒷방향으로 똑같은거 채우기
 # train2 = train2.fillna(train2.bfill) 
 
+train1 = train1.sort_values(['일자'],ascending=True)
+train2 = train2.sort_values(['일자'],ascending=True)
+
+# train1_test = train1['시가'][-5:]
+# train2_test = train2['종가'][-5:]
+
+timesteps = 6
+adds = 2
 # 나누기
+def split_xy(dataFrame, cutting_size, y_behind_size,  y_column):
+    xs = []
+    ys = [] 
+    for i in range(len(dataFrame) - cutting_size - y_behind_size):
+        x = dataFrame[i : (i + cutting_size)]
+        y = dataFrame[i + cutting_size + y_behind_size : (i + cutting_size + y_behind_size + 1) ][y_column]
+        xs.append(x)
+        ys.append(y)
+    return (np.array(xs), np.array(ys))
+
+
+train1 = train1[train1.index > '2018/05/04']
+train2 = train2[train2.index > '2018/05/04']
+
 timestep = 6
-add = 1
+add = 2
 def split(train1,train2,timestep,col1,col2,add) : 
     x=[]
     y=[]
@@ -79,10 +89,15 @@ def split(train1,train2,timestep,col1,col2,add) :
     return np.array(x) , np.array(y) , np.array(z) , np.array(f)
 
 
-
-
 x1 , y1 , x2, y2 = split(train1,train2,timestep,'시가','종가',add)
 
+# 샘플 추출 x
+compare_predict_size = 10
+samsung_sample_x = x1[-compare_predict_size:]
+amore_sample_x = x2[-compare_predict_size :]
+# 샘플 추출 y
+samsung_sample_y = np.append(y1[-compare_predict_size + 2:], ["24/02/06 시가","24/02/07 시가"]) 
+amore_sample_y = np.append(y2[-compare_predict_size + 2:], ["24/02/06 종가","24/02/07 종가"])
 # iloc = index 를 제외한 수치(값)들만을 뽑아내준다. 
 
 
@@ -98,6 +113,9 @@ train2 = np.array(train2)
 # y1 = y1[end_row:]
 # y2 = y2[end_row2:]
 
+
+
+
 # print(x1.shape)       # (1418, 6, 16)
 # print(x2)       # (1418, 6, 16)
 # print(y1.shape)       # (1418,)
@@ -106,8 +124,11 @@ train2 = np.array(train2)
 
 
 
+
+
+
 x1_train , x1_test , x2_train , x2_test , y1_train , y1_test ,y2_train,y2_test = train_test_split(x1,x2,y1,y2,
-                                                                                  test_size=0.3 , random_state= 730320 , shuffle=True )
+                                                                                  test_size=0.3 , random_state= 220118 , shuffle=True )
 
 
 print(x1_train.shape)       # (992, 6, 16)
@@ -119,6 +140,8 @@ x1_train = x1_train.reshape(-1,96)
 x2_train = x2_train.reshape(-1,96)
 x1_test = x1_test.reshape(-1,96)
 x2_test = x2_test.reshape(-1,96)
+r_samsung_sample_x = samsung_sample_x.reshape(samsung_sample_x.shape[0], samsung_sample_x.shape[1] * samsung_sample_x.shape[2])
+r_amore_sample_x = amore_sample_x.reshape(amore_sample_x.shape[0], amore_sample_x.shape[1] * amore_sample_x.shape[2])
 
 
 # scaler 사용
@@ -131,11 +154,22 @@ x1_train = scaler.fit_transform(x1_train)
 x2_train = scaler.fit_transform(x2_train)
 x1_test = scaler.fit_transform(x1_test)
 x2_test = scaler.fit_transform(x2_test)
+r_samsung_sample_x = scaler.transform(r_samsung_sample_x)
+r_amore_sample_x = scaler.transform(r_amore_sample_x)
 
 x1_train = x1_train.reshape(-1,6,16)
 x2_train = x2_train.reshape(-1,6,16)
 x1_test = x1_test.reshape(-1,6,16)
 x2_test = x2_test.reshape(-1,6,16)
+
+# print()
+
+samsung_sample_x = r_samsung_sample_x.reshape(-1, samsung_sample_x.shape[1], samsung_sample_x.shape[2])
+amore_sample_x = r_amore_sample_x.reshape(-1, amore_sample_x.shape[1], amore_sample_x.shape[2])
+
+print(samsung_sample_x.shape)           # (10, 6, 16, 1)
+print(amore_sample_x.shape)             # (10, 6, 16, 1)
+
 
 
 model = load_model('c:/_data/sihum/sihum.h5')
@@ -150,16 +184,32 @@ r21 = r2_score(y1_test,pre[0])
 r22 = r2_score(y2_test,pre[1])
 
 
-for i in range(5):
-    print('실제' , train1_test[i] , '예측', pre[0][i] )
+# for i in range(5):
+#     print('실제' , train1_test[i] , '예측', pre[0][i] )
 
-for i in range(5):
-    print('실제' , train2_test[i] , '예측', pre[1][i] )
+# for i in range(5):
+#     print('실제' , train2_test[i] , '예측', pre[1][i] )
 
+print("="*100)
+sample_dataset_y = [samsung_sample_y,amore_sample_y]
+sample_predict_x = model.predict([samsung_sample_x,amore_sample_x])
+
+
+
+
+for i in range(len(sample_dataset_y)):
+    if i == 0 :
+        print("\t\tSAMSUNG\t시가")
+    else:
+        print("="*100)
+        print("\t\tAMORE\t종가")
+    for j in range(compare_predict_size):
+        print(f"\tD-{compare_predict_size - j  - 1}: {sample_dataset_y[i][j]}\t예측값 {np.round(sample_predict_x[i][j])}\t")
+print("="*100)
 
 
 print('loss',loss)
-print('시가',pre[0][0])
-print('종가',pre[1][0])
+# print('시가',pre[0][0])
+# print('종가',pre[1][0])
 print('시가 r2',r21)
 print('종가 r2',r22)
