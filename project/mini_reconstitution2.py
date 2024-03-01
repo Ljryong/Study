84904e439e83c61fe58f1bfde75ad3385034a8b1
Ljryong <aa5357@naver.com>
2024-03-01 22:15:01 +0900

    -

--- project/mini_reconstitution2.py
+++ project/mini_reconstitution2.py
@@ -374,7 +374,7 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 # Create a callback that saves the model's weights
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  save_weights_only=True,
-                                                 verbose=1)     
+                                                 verbose=1)
 
 #=============================================================================================================#
 
