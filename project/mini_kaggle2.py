1be4e7471eccd181200b8e4d7d62e5146f1e434a
Ljryong <aa5357@naver.com>
2024-03-02 20:50:27 +0900

    mini project
    kaggle에 있는 비슷한 코드를 가지고와서 인용

---
+++ project/mini_kaggle2.py
@@ -0,0 +1,521 @@
+import os
+import tqdm
+import random
+import pathlib
+import imageio
+import itertools
+import collections
+
+import cv2
+import numpy as np
+import remotezip as rz
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+from tensorflow_docs.vis import embed
+
+import keras
+import tensorflow as tf
+import tensorflow_hub as hub
+from keras import layers
+from keras.optimizers import Adam
+from keras.losses import SparseCategoricalCrossentropy
+
+from official.projects.movinet.modeling import movinet
+from official.projects.movinet.modeling import movinet_model
+from official.projects.movinet.tools import export_saved_model
+import warnings
+
+NUM_THREADS = 22  # 사용하고 싶은 CPU 쓰레드 수
+
+tf.config.threading.set_intra_op_parallelism_threads(NUM_THREADS)
+tf.config.threading.set_inter_op_parallelism_threads(NUM_THREADS)
+
+warnings.filterwarnings("ignore", category=UserWarning, module="tensorflow")
+
+
+def get_class(fname):
+  """
+    Retrieve the name of the class given a filename.
+
+    Args:
+      fname: Name of the file in the UCF101 dataset.
+
+    Return:
+      Class that the file belongs to.
+  """
+  return fname.split('_')[-3]
+
+def get_files_per_class(files):
+  """
+    Retrieve the files that belong to each class.
+
+    Args:
+      files: List of files in the dataset.
+
+    Return:
+      Dictionary of class names (key) and files (values).
+  """
+  files_for_class = collections.defaultdict(list)
+  for fname in files:
+    class_name = get_class(fname)
+    files_for_class[class_name].append(fname)
+  return files_for_class
+
+def download_from_zip(zip_url, to_dir, file_names):
+  """
+    Download the contents of the zip file from the zip URL.
+
+    Args:
+      zip_url: Zip URL containing data.
+      to_dir: Directory to download data to.
+      file_names: Names of files to download.
+  """
+  with rz.RemoteZip(zip_url) as zip:
+    for fn in tqdm.tqdm(file_names):
+      class_name = get_class(fn)
+      zip.extract(fn, str(to_dir / class_name))
+      unzipped_file = to_dir / class_name / fn
+
+      fn = pathlib.Path(fn).parts[-1]
+      output_file = to_dir / class_name / fn
+      unzipped_file.rename(output_file,)
+
+def split_class_lists(files_for_class, count):
+  """
+    Returns the list of files belonging to a subset of data as well as the remainder of
+    files that need to be downloaded.
+
+    Args:
+      files_for_class: Files belonging to a particular class of data.
+      count: Number of files to download.
+
+    Return:
+      split_files: Files belonging to the subset of data.
+      remainder: Dictionary of the remainder of files that need to be downloaded.
+  """
+  split_files = []
+  remainder = {}
+  for cls in files_for_class:
+    split_files.extend(files_for_class[cls][:count])
+    remainder[cls] = files_for_class[cls][count:]
+  return split_files, remainder
+
+def download_ufc_101_subset(zip_url, num_classes, splits, download_dir):
+  """
+    Download a subset of the UFC101 dataset and split them into various parts, such as
+    training, validation, and test.
+
+    Args:
+      zip_url: Zip URL containing data.
+      num_classes: Number of labels.
+      splits: Dictionary specifying the training, validation, test, etc. (key) division of data
+              (value is number of files per split).
+      download_dir: Directory to download data to.
+
+    Return:
+      dir: Posix path of the resulting directories containing the splits of data.
+  """
+#   files = list_files_per_class(zip_url)
+#   for f in files:
+#     tokens = f.split('/')
+#     if len(tokens) <= 2:
+#       files.remove(f) # Remove that item from the list if it does not have a filename
+
+#   files_for_class = get_files_per_class(files)
+
+  classes = list(files_for_class.keys())[:num_classes]
+
+  for cls in classes:
+    new_files_for_class = files_for_class[cls]
+    random.shuffle(new_files_for_class)
+    files_for_class[cls] = new_files_for_class
+
+  # Only use the number of classes you want in the dictionary
+  files_for_class = {x: files_for_class[x] for x in list(files_for_class)[:num_classes]}
+
+  dirs = {}
+  for split_name, split_count in splits.items():
+    print(split_name, ":")
+    split_dir = download_dir / split_name
+    split_files, files_for_class = split_class_lists(files_for_class, split_count)
+    download_from_zip(zip_url, split_dir, split_files)
+    dirs[split_name] = split_dir
+
+  return dirs
+
+def format_frames(frame, output_size):
+  """
+    Pad and resize an image from a video.
+
+    Args:
+      frame: Image that needs to resized and padded.
+      output_size: Pixel size of the output frame image.
+
+    Return:
+      Formatted frame with padding of specified output size.
+  """
+  frame = tf.image.convert_image_dtype(frame, tf.float32)
+  frame = tf.image.resize_with_pad(frame, *output_size)
+  return frame
+
+def frames_from_video_file(video_path, n_frames, output_size = (256,256), frame_step = 15):
+  """
+    Creates frames from each video file present for each category.
+
+    Args:
+      video_path: File path to the video.
+      n_frames: Number of frames to be created per video file.
+      output_size: Pixel size of the output frame image.
+
+    Return:
+      An NumPy array of frames in the shape of (n_frames, height, width, channels).
+  """
+  # Read each video frame by frame
+  result = []
+  src = cv2.VideoCapture(str(video_path))
+
+  video_length = src.get(cv2.CAP_PROP_FRAME_COUNT)
+
+  need_length = 1 + (n_frames - 1) * frame_step
+
+  if need_length > video_length:
+    start = 0
+  else:
+    max_start = video_length - need_length
+    start = random.randint(0, max_start + 1)
+
+  src.set(cv2.CAP_PROP_POS_FRAMES, start)
+  # ret is a boolean indicating whether read was successful, frame is the image itself
+  ret, frame = src.read()
+  result.append(format_frames(frame, output_size))
+
+  for _ in range(n_frames - 1):
+    for _ in range(frame_step):
+      ret, frame = src.read()
+    if ret:
+      frame = format_frames(frame, output_size)
+      result.append(frame)
+    else:
+      result.append(np.zeros_like(result[0]))
+  src.release()
+  result = np.array(result)[..., [2, 1, 0]]
+
+  return result
+
+def to_gif(images):
+  converted_images = np.clip(images * 255, 0, 255).astype(np.uint8)
+  imageio.mimsave('./animation.gif', converted_images, fps=10)
+  return embed.embed_file('./animation.gif')
+
+
+class FrameGenerator:
+  def __init__(self, path, n_frames, training = False):
+    """ Returns a set of frames with their associated label.
+
+      Args:
+        path: Video file paths.
+        n_frames: Number of frames.
+        training: Boolean to determine if training dataset is being created.
+    """
+    self.path = path
+    self.n_frames = n_frames
+    self.training = training
+    self.class_names = sorted(set(p.name for p in self.path.iterdir() if p.is_dir()))
+    self.class_ids_for_name = dict((name, idx) for idx, name in enumerate(self.class_names))
+
+  def get_files_and_class_names(self):
+    video_paths = list(self.path.glob('*/*.mp4'))
+    classes = [p.parent.name for p in video_paths]
+    return video_paths, classes
+
+  def __call__(self):
+    video_paths, classes = self.get_files_and_class_names()
+
+    pairs = list(zip(video_paths, classes))
+
+    if self.training:
+      random.shuffle(pairs)
+
+    for path, name in pairs:
+      video_frames = frames_from_video_file(path, self.n_frames)
+      label = self.class_ids_for_name[name] # Encode labels
+      yield video_frames, label
+      
+      
+from pathlib import Path
+# download_dir = pathlib.Path('/kaggle/input/sign-language/data')
+# download_dir = pathlib.Path("C:\\Users\\AIA\\Desktop\\ai\\AI_mini_project\\resource\\download_dir")
+download_dir = "C:/prodownload"
+
+subset_paths = {}
+subset_paths['train'] = Path(os.path.join(download_dir,"train"))
+subset_paths['val'] = Path(os.path.join(download_dir,"val"))
+subset_paths['test'] = Path(os.path.join(download_dir,"test"))
+
+# print(subset_paths['train'])
+
+def format_frames(frame, output_size):
+  """
+    Pad and resize an image from a video.
+
+    Args:
+      frame: Image that needs to resized and padded.
+      output_size: Pixel size of the output frame image.
+
+    Return:
+      Formatted frame with padding of specified output size.
+  """
+  frame = tf.image.convert_image_dtype(frame, tf.float32)
+  frame = tf.image.resize_with_pad(frame, *output_size)
+  return frame
+
+def frames_from_video_file(video_path, n_frames, output_size = (256,256), frame_step = 2):
+  """
+    Creates frames from each video file present for each category.
+
+    Args:
+      video_path: File path to the video.
+      n_frames: Number of frames to be created per video file.
+      output_size: Pixel size of the output frame image.
+
+    Return:
+      An NumPy array of frames in the shape of (n_frames, height, width, channels).
+  """
+  # Read each video frame by frame
+  result = []
+  src = cv2.VideoCapture(str(video_path))
+
+  video_length = src.get(cv2.CAP_PROP_FRAME_COUNT)
+
+  need_length = 1 + (n_frames - 1) * frame_step
+
+  if need_length > video_length:
+    start = 0
+  else:
+    max_start = video_length - need_length
+    start = random.randint(0, max_start + 1)
+
+  src.set(cv2.CAP_PROP_POS_FRAMES, start)
+  # ret is a boolean indicating whether read was successful, frame is the image itself
+  ret, frame = src.read()
+  result.append(format_frames(frame, output_size))
+
+  for _ in range(n_frames - 1):
+    for _ in range(frame_step):
+      ret, frame = src.read()
+    if ret:
+      frame = format_frames(frame, output_size)
+      result.append(frame)
+    else:
+      result.append(np.zeros_like(result[0]))
+  src.release()
+  result = np.array(result)[..., [2, 1, 0]]
+
+  return result
+class FrameGenerator:
+  def __init__(self, path, n_frames, training = False):
+    """ Returns a set of frames with their associated label.
+
+      Args:
+        path: Video file paths.
+        n_frames: Number of frames.
+        training: Boolean to determine if training dataset is being created.
+    """
+    self.path = path
+    self.n_frames = n_frames
+    self.training = training
+    self.class_names = sorted(set(p.name for p in self.path.iterdir() if p.is_dir()))
+    self.class_ids_for_name = dict((name, idx) for idx, name in enumerate(self.class_names))
+
+  def get_files_and_class_names(self):
+    video_paths = list(self.path.glob('*/*.mp4'))
+    video_paths += list(self.path.glob('*/*.avi'))
+    
+    classes = [p.parent.name for p in video_paths]
+    return video_paths, classes
+
+  def __call__(self):
+    video_paths, classes = self.get_files_and_class_names()
+
+    pairs = list(zip(video_paths, classes))
+
+    if self.training:
+      random.shuffle(pairs)
+
+    for path, name in pairs:
+      video_frames = frames_from_video_file(path, self.n_frames)
+      label = self.class_ids_for_name[name] # Encode labels
+      yield video_frames, label
+fg = FrameGenerator(subset_paths['train'], 15, training=True)
+
+frames, label = next(fg())
+
+print(f"Shape: {frames.shape}")
+print(f"Label: {label}")
+
+batch_size = 1
+num_frames = 100
+
+CLASSES = sorted(os.listdir("C:\prodownload\\train"))
+# CLASSES = sorted(os.listdir("C:\\Users\\AIA\\Desktop\\ai\\AI_mini_project\\resource\\download_dir\\train"))
+
+output_signature = (tf.TensorSpec(shape = (None, None, None, 3), dtype = tf.float32),
+                    tf.TensorSpec(shape = (), dtype = tf.int16))
+
+train_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train'], num_frames, training = True),
+                                          output_signature = output_signature)
+train_ds = train_ds.batch(batch_size)
+
+val_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['val'], num_frames),
+                                          output_signature = output_signature)
+val_ds = val_ds.batch(batch_size)
+
+test_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['test'], num_frames),
+                                         output_signature = output_signature)
+test_ds = test_ds.batch(batch_size)
+
+# print(CLASSES)
+
+for frames, labels in train_ds.take(1):
+  print(f"Shape: {frames.shape}")
+  print(f"Label: {labels.shape}")
+  
+  
+model_id = 'a3'
+use_positional_encoding = model_id in {'a3', 'a4', 'a5'}
+resolution = 256
+
+backbone = movinet.Movinet(
+    model_id=model_id,
+    causal=True,
+    conv_type='2plus1d',
+    se_type='2plus3d',
+    activation='hard_swish',
+    gating_activation='hard_sigmoid',
+    use_positional_encoding=use_positional_encoding,
+    use_external_states=False,
+)
+
+model = movinet_model.MovinetClassifier(
+    backbone,
+    num_classes=600,
+    output_states=True)
+
+# Create your example input here.
+# Refer to the paper for recommended input shapes.
+inputs = tf.ones([1, 15, 256, 256, 3])
+
+# [Optional] Build the model and load a pretrained checkpoint.
+model.build(inputs.shape)
+
+# # Extract pretrained weights
+# !wget https://storage.googleapis.com/tf_model_garden/vision/movinet/movinet_a3_stream.tar.gz -O movinet_a3_stream.tar.gz -q
+# !tar -xvf movinet_a3_stream.tar.gz
+
+checkpoint_dir = 'D:\\movinet_a3_stream'
+checkpoint_path = tf.train.latest_checkpoint(checkpoint_dir)
+checkpoint = tf.train.Checkpoint(model=model)
+status = checkpoint.restore(checkpoint_path)
+status.assert_existing_objects_matched()
+
+
+# # Detect hardware
+# try:
+#   tpu_resolver = tf.distribute.cluster_resolver.TPUClusterResolver() # TPU detection
+# except ValueError:
+#   tpu_resolver = None
+#   gpus = tf.config.experimental.list_logical_devices("GPU")
+
+# # Select appropriate distribution strategy
+# if tpu_resolver:
+#   tf.config.experimental_connect_to_cluster(tpu_resolver)
+#   tf.tpu.experimental.initialize_tpu_system(tpu_resolver)
+#   distribution_strategy = tf.distribute.experimental.TPUStrategy(tpu_resolver)
+#   print('Running on TPU ', tpu_resolver.cluster_spec().as_dict()['worker'])
+# elif len(gpus) > 1:
+#   distribution_strategy = tf.distribute.MirroredStrategy([gpu.name for gpu in gpus])
+#   print('Running on multiple GPUs ', [gpu.name for gpu in gpus])
+# elif len(gpus) == 1:
+#   distribution_strategy = tf.distribute.get_strategy() # default strategy that works on CPU and single GPU
+#   print('Running on single GPU ', gpus[0].name)
+# else:
+#   distribution_strategy = tf.distribute.get_strategy() # default strategy that works on CPU and single GPU
+#   print('Running on CPU')
+
+# print("Number of accelerators: ", distribution_strategy.num_replicas_in_sync)
+
+def build_classifier(batch_size, num_frames, resolution, backbone, num_classes):
+  """Builds a classifier on top of a backbone model."""
+  model = movinet_model.MovinetClassifier(
+      backbone=backbone,
+      num_classes=num_classes)
+  model.build([batch_size, num_frames, resolution, resolution, 3])
+
+  return model
+
+num_classes = len(CLASSES)
+print("num_classes : ", num_classes)
+# Construct loss, optimizer and compile the model
+#with distribution_strategy.scope():
+model = build_classifier(batch_size, num_frames, resolution, backbone, num_classes )
+loss_obj = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+model.compile(loss=loss_obj, optimizer=optimizer, metrics=['accuracy'])
+
+checkpoint_path = "trained_model/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+results = model.fit(train_ds,
+                    validation_data=val_ds,
+                    epochs=10,
+                    validation_freq=1,
+                    verbose=1,
+                    callbacks=[cp_callback])
+
+model.evaluate(test_ds)
+
+def get_actual_predicted_labels(dataset):
+  """
+    Create a list of actual ground truth values and the predictions from the model.
+
+    Args:
+      dataset: An iterable data structure, such as a TensorFlow Dataset, with features and labels.
+
+    Return:
+      Ground truth and predicted values for a particular dataset.
+  """
+  actual = [labels for _, labels in dataset.unbatch()]
+  predicted = model.predict(dataset)
+
+  actual = tf.stack(actual, axis=0)
+  predicted = tf.concat(predicted, axis=0)
+  predicted = tf.argmax(predicted, axis=1)
+
+  return actual, predicted
+
+def plot_confusion_matrix(actual, predicted, labels, ds_type):
+  cm = tf.math.confusion_matrix(actual, predicted)
+  ax = sns.heatmap(cm, annot=True, fmt='g')
+  sns.set(rc={'figure.figsize':(6, 16)})
+  sns.set(font_scale=1.4)
+  ax.set_title('Confusion matrix of action recognition for ' + ds_type)
+  ax.set_xlabel('Predicted Action')
+  ax.set_ylabel('Actual Action')
+  plt.xticks(rotation=90)
+  plt.yticks(rotation=0)
+  ax.xaxis.set_ticklabels(labels)
+  ax.yaxis.set_ticklabels(labels)
+  plt.show()
+  return cm
+
+fg = FrameGenerator(subset_paths['train'], num_frames, training = True)
+label_names = list(fg.class_ids_for_name.keys())
+
+actual, predicted = get_actual_predicted_labels(test_ds)
+cm = plot_confusion_matrix(actual, predicted, label_names, 'test')
