eb87de3216fbcfcdcb93889b60dd1279a68f246d
Ljryong <aa5357@naver.com>
2024-03-11 12:52:56 +0900

    AI factory
    ai factory 산불 문제 풀기
    모델별로 풀어보기

---
+++ AI_factory/sam.py
@@ -0,0 +1,7 @@
+import segmentation_models as sm
+# Segmentation Models: using `keras` framework.
+import keras
+# or from tensorflow import keras
+
+keras.backend.set_image_data_format('channels_last')
+# or keras.backend.set_image_data_format('channels_first')uy
