6a1502b541bf5d7ca3056ecb7e057e7df4d4a6c3
Ljryong <aa5357@naver.com>
2024-03-26 16:38:12 +0900

    Update tf05_Variable.py
    변수는 항상 초기화를 시키고 사용해줘야한다

--- TF_1.14/tf05_Variable.py
+++ TF_1.14/tf05_Variable.py
@@ -4,7 +4,7 @@ sess = tf.compat.v1.Session()
 a = tf.Variable( [2], dtype=tf.float32 )
 b = tf.Variable( [3], dtype=tf.float32 ) 
 
-init = tf.compat.v1.global_variables_initializer()                 # 변수 초기화
+init = tf.compat.v1.global_variables_initializer()                 # 변수 초기화 / 항상 초기화 해줘야한다
 
 sess.run(init)      # 초기화도 run으로 시켜줘야 됨
 
