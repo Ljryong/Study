8aae9b4099ae513ffe8cf1709dbb4053c53a9193
Ljryong <aa5357@naver.com>
2024-04-15 09:38:12 +0900

    Tensor 즉시실행모드
    Tensor 즉시실행모드

--- TF_1.14/tf03_eager.py
+++ TF_1.14/tf03_eager.py
@@ -23,3 +23,18 @@ print(sess.run(hello))
 # 1.14.0          enable                   에러(불가능)
 # 2.9.0           disable                  가능     ★★★★    tensorflow2 환경에서 tensor 1 코드를 쓸 일이 있을 경우 사용
 # 2.9.0           enable (디폴트)          에러(불가능)
+
+""" 
+Tensor 1 은 '그래프연산' 모드
+Tensor 2 는 '즉시실행' 모드
+
+tf.compat.v1.enable_eager_execution() 즉시실행모드 켜
+                        -> Tensor 2 의 디폴트
+
+tf.compat.v1.disable_eager_execution() 즉시실행모드 꺼 
+                                -> 그래프 연산모드로 돌아간다.
+                                -> Tensor 1 코드를 쓸 수 있다
+
+tf.executing_eagerly()  True면 즉시실행모드, -> Tensor 2 코드만 써야한다.
+                        False면 그래프연산모드 -> Tensor 1 코드를 쓸 수 있다
+"""
