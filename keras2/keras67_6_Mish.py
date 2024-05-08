c1c170bdae65a9f661aec2a3b04836e999b86ed4
Ljryong <aa5357@naver.com>
2024-05-08 17:58:35 +0900

    Mish 함수 만들기
    Mish 함수 만들기

---
+++ keras2/keras67_6_Mish.py
@@ -0,0 +1,20 @@
+# SiLu(Sigmoid-Weighted Linear Unit) = Swish
+
+# 음수를 다 버리기 아까워서 생김
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-5,5,0.1)
+
+def mish(x) :
+    return x * np.tanh(np.log(1+np.exp(x)))     # x * tanh(softplus(x))
+                                                # softplus(x) = log(1 + exp(x))
+
+# mish = lambda x : x * np.tanh(np.log(1+np.exp(x)))
+
+y = mish(x)
+
+plt.plot(x,y)
+plt.grid()
+plt.show()
